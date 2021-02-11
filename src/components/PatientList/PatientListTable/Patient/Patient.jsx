1cb963611cac175b1ba4177cacfdf847d14780bb
Yuriy <agent250691@yandex.ru>
2021-02-11 18:31:34 +0300

    feat: add hover on row in patient table

--- src/components/PatientList/PatientListTable/Patient/Patient.jsx
+++ src/components/PatientList/PatientListTable/Patient/Patient.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Patient = (props) => {
   const fullName = props.firstName + ' ' + props.lastName + ' ' + props.patrName
   return (
-    <tr onClick={() => props.handleClick(props.birthDate, fullName, props.diagnosis)}>
+    <tr className="patientRow" onClick={() => props.handleClick(props.birthDate, fullName, props.diagnosis)}>
       <td>{props.index}</td>
       <td>{fullName}</td>
       <td>{props.active ? props.bedNumber : props.cause}</td>
